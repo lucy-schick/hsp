f2a43d68885875621f7edf3f505a96df71d6d1fe
almac2022 <al@newgraphenvironment.com>
2024-01-31 16:41:26 -0800

    move to devtools

--- scripts/packages.R
+++ scripts/packages.R
@@ -1,100 +1,29 @@
-# # install.packages('pacman')
-#
-# pacman::p_load(
-#   tidyverse,
-#   readwritesqlite,
-#   sf,
-#   readxl,
-#   janitor,
-#   leafem,
-#   leaflet,
-#   plotKML,
-#   kableExtra,
-#   httr,
-#   RPostgres,
-#   RPostgreSQL,
-#   DBI,
-#   magick,
-#   bcdata,
-#   jpeg,
-#   datapasta,
-#   knitr,
-#   data.table,
-#   lubridate,
-#   forcats,
-#   bookdown,
-#   fasstr,
-#   # flextable,
-#   english,
-#   leaflet.extras,
-#   ggdark,
-#   pdftools,
-#   geojsonsf,
-#   bit64 ##to make integer column type for pg
-#   # gert  ##to track git moves
-#   ##leafpop I think
-#   )
+# install.packages('pak')
 
-
-
-# install.packages('pacman')
-
-
-package_list <- c(
-  # plotKML,  #plot kml needed to go ahead of other packages for some reason and wants to reinstall everytime.... not sure why. hash out for now
-  'raster', #load this dog before dplyr yo
+pkgs_cran <- c(
   'tidyverse',
-  'readwritesqlite',
-  'sf',
-  'readxl',
-  'janitor',
-  'leafem',
-  'leaflet',
-  'kableExtra',
-  'httr',
-  'RPostgres',
-  'RPostgreSQL',
-  'DBI',
-  'magick',
-  'bcdata',
-  'jpeg',
-  'datapasta',
   'knitr',
-  'data.table',
-  'lubridate',
-  'forcats',
   'bookdown',
-  'fasstr',
-  'tidyhydat',
-  'elevatr',
-  'rayshader',
-  'exifr',
-  # flextable,
-  'english',
-  'leaflet.extras',
-  'ggdark',
-  'geojsonio',
-  'pdftools',
-  'xlsx',
-  'analogsea'
-  # rgl,
-  # geojsonsf,
-  # bit64 ##to make integer column type for pg
-  # gert  ##to track git moves
-  ##leafpop I think
+  'rmarkdown',
+  'pagedown'
 )
-lapply(package_list,
+
+pkgs_gh <- c(
+  "newgraphenvironment/fpr"
+)
+
+pkgs_all <- c(pkgs_cran,
+              pkgs_gh)
+
+
+# install or upgrade all the packages with pak
+lapply(pkgs_all,
+       pak::pkg_install, dependencies = TRUE, ask = FALSE)
+
+# load all the packages
+pkgs_ld <- c(pkgs_cran,
+             basename(pkgs_gh))
+
+lapply(pkgs_ld,
        require,
        character.only = TRUE)
-
-# we need the development version of pagedown as of 20200303 https://github.com/rstudio/pagedown/issues/265
-# remotes::install_github('rstudio/pagedown')
-
-
-pacman::p_load_gh("poissonconsulting/fwapgr",
-                  'poissonconsulting/poisspatial',
-                  "crsh/citr",
-                  'rstudio/pagedown',
-                  "poissonconsulting/fishbc",
-                  "newgraphenvironment/fpr")
-# "poissonconsulting/subfoldr2")
